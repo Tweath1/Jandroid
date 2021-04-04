efa6f8ad6ae98eeeffef6226232f8fc982ff186d
Ted Weathersbee <tweath1@umbc.edu>
2021-04-04 05:37:56 -0400

    added embeds and added the percentage system to grab much more info for the user. The coding is probably significantly more manual forcing than it needs to be but hopefully will be cleaned up later

---
+++ godAbbreviations.py
@@ -0,0 +1,157 @@
+
+
+def godAbbreviations(godName):
+    godName = godName.lower()
+    if godName == "amc" or godName == "ah muzen cab":
+        godName = "ah-muzen-cab"
+    elif godName == "ap" or godName == "ah puch":
+        godName = "ah-puch"
+    elif godName == "ama":
+        godName = "amaterasu"
+    elif godName == "goobis":
+        godName = "anubis"
+    elif godName == "ao" or godName == "ak" or godName == "ao kuang":
+        godName = "ao-kuang"
+    elif godName == "aphro":
+        godName = "aphrodite"
+    elif godName == "ara":
+        godName = "arachne"
+    elif godName == "art":
+        godName = "artemis"
+    elif godName == "baba" or godName == "baba yaga":
+        godName = "baba-yaga"
+    elif godName == "bacc":
+        godName = "bacchus"
+    elif godName == "baka":
+        godName = "bakasura"
+    elif godName == "baron" or godName == "baron samedi":
+        godName = "baron-samedi"
+    elif godName == "bast":
+        godName = "bastet"
+    elif godName == "bell":
+        godName = "bellona"
+    elif godName == "cab" or godName == "cabra":
+        godName = "cabrakan"
+    elif godName == "cama":
+        godName = "camazotz"
+    elif godName == "cerb":
+        godName = "cerberus"
+    elif godName == "cern":
+        godName = "cernunnos"
+    elif godName == "change" or godName == "chang'e":
+        godName = "chang-e"
+    elif godName == "chern" or godName == "cherno":
+        godName = "chernobog"
+    elif godName == "lulu":
+        godName = "cthulu"
+    elif godName == "cu" or godName == "cu chu" or godName == "cu chulainn" or godName == "chu chu":
+        godName = "cu-chulainn"
+    elif godName == "daji" or godName == "da ji":
+        godName = "da-ji"
+    elif godName == "danza":
+        godName = "danzaburou"
+    elif godName == "disco":
+        godName = "discordia"
+    elif godName == "erlang" or godName == "erlang shen":
+        godName = "erlang-shen"
+    elif godName == "isis":
+        godName = "eset"
+    elif godName == "faf":
+        godName = "fafnir"
+    elif godName == "fen":
+        godName = "fenrir"
+    elif godName == "ganesh":
+        godName = "ganesha"
+    elif godName == "gil" or godName == "gilga":
+        godName = "gilgamesh"
+    elif godName == "guan" or godName == "guan yu":
+        godName = "guan-yu"
+    elif godName == "hachi":
+        godName = "hachiman"
+    elif godName == "hebo" or godName == "he bo":
+        godName = "he-bo"
+    elif godName == "heim":
+        godName = "heimdallr"
+    elif godName == "herc":
+        godName = "hercules"
+    elif godName == "hou" or godName == "hou yi":
+        godName = "hou-yi"
+    elif godName == "hun" or godName == "batz" or godName == "hun batz":
+        godName = "hun-batz"
+    elif godName == "iza":
+        godName = "izanami"
+    elif godName == "jing" or godName == "jing wei":
+        godName = "jing-wei"
+    elif godName == "jorm":
+        godName = "jormungandr"
+    elif godName == "khep":
+        godName = "khepri"
+    elif godName == "king" or godName == "arthur" or godName == "king arthur" or godName == "ka":
+        godName = "king-arthur"
+    elif godName == "kuku":
+        godName = "kukulkan"
+    elif godName == "kumbha" or godName == "kumba":
+        godName = "kumbhakarna"
+    elif godName == "kuzen":
+        godName = "kuzenbo"
+    elif godName == "dusa":
+        godName = "medusa"
+    elif godName == "merc":
+        godName = "mercury"
+    elif godName == "nezha" or godName == "ne zha":
+        godName = "ne-zha"
+    elif godName == "nem":
+        godName = "nemesis"
+    elif godName == "nuwa" or godName == "nu wa":
+        godName = "nu-wa"
+    elif godName == "olo":
+        godName = "olorun"
+    elif godName == "osi":
+        godName = "osiris"
+    elif godName == "pers" or godName == "perse":
+        godName = "persephone"
+    elif godName == "pos" or godName == "pose":
+        godName = "poseidon"
+    elif godName == "ram":
+        godName = "rama"
+    elif godName == "rat":
+        godName = "ratatoskr"
+    elif godName == "rav" or godName == "ravan":
+        godName = "ravana"
+    elif godName == "serq":
+        godName = "serqet"
+    elif godName == "swk" or godName == "wukong" or godName == "sun wukong":
+        godName = "sun-wukong"
+    elif godName == "susan" or godName == "sus":
+        godName = "susano"
+    elif godName == "sylv"or godName == "tree":
+        godName = "sylvanus"
+    elif godName == "thana" or godName == "than":
+        godName = "thanatos"
+    elif godName == "morrigan" or godName == "morri" or godName == "the morrigan":
+        godName = "the-morrigan"
+    elif godName == "tia":
+        godName = "tiamat"
+    elif godName == "tsuku" or godName == "tsuki" or godName == "tsu":
+        godName = "tsukuyomi"
+    elif godName == "vam" or godName == "vaman":
+        godName = "vamana"
+    elif godName == "xbal":
+        godName = "xbalanque"
+    elif godName == "xing" or godName == "xing tian" or godName == "XT":
+        godName = "xing-tian"
+    elif godName == "yem":
+        godName = "yemoja"
+    elif godName == "zhong" or godName == "zhong kui":
+        godName = "zhong-kui"
+
+    return godName
+
+if __name__ == '__main__':
+
+    name = "nu wa"
+    fixedName = godAbbreviations(name)
+    print(fixedName)
+
+
+
--- jandroid.py
+++ jandroid.py
@@ -1,19 +1,68 @@
 import discord
 import requests
 from bs4 import BeautifulSoup
+import math
+from godAbbreviations import godAbbreviations
 
 with open('token.txt', 'r') as file:
-  thing = file.readline()
+  token = file.readline()
 
 
-def split_by_char(string):
-    charList = []
-    for char in string:
-        charList.append(char)
-    return charList
+def createItemBuildEmbed(godName, itemString):
+    embedVar = discord.Embed(color=0x7fffd4)
+    #print("https://static.smite.guru/i/champions/icons/{}.jpg".format(godName))
+    embedVar.set_thumbnail(url="https://static.smite.guru/i/champions/icons/{}.jpg".format(godName))
+    embedVar.set_author(name=godName, url="https://smite.guru/builds/{}".format(godName), icon_url="https://static.smite.guru/i/champions/icons/{}.jpg".format(godName))
+    embedVar.add_field(name="Here are the 6 most popular items for {}".format(godName), value=itemString, inline=True)
+    return embedVar
 
 
-def math(equation):
+def createGuidedBuildEmbed(godName, itemList, percentageList):
+    embedVar = discord.Embed(color=0x7fffd4)
+    starterList = [itemList[0], itemList[1], itemList[2]]
+    bootsList = [itemList[3], itemList[4], itemList[5]]
+    relicList = [itemList[6], itemList[7], itemList[8], itemList[9]]
+    coreItemsList = [itemList[10], itemList[11], itemList[12], itemList[13]]
+    starterPercentageList = [percentageList[0], percentageList[1], percentageList[2]]
+    bootsPercentageList = [percentageList[3], percentageList[4], percentageList[5]]
+    relicPercentageList = [percentageList[6], percentageList[7], percentageList[8], percentageList[9]]
+    coreItemsPercentageList = [percentageList[10], percentageList[11], percentageList[12], percentageList[13]]
+    for index in range(len(relicList) - 1, -1, -1):
+        if relicList[index] == "Vision Shard":
+            relicList.pop(index)
+            relicPercentageList.pop(index)
+    print(starterList)
+    print(bootsList)
+    print(relicList)
+    print(coreItemsList)
+    print(starterPercentageList)
+    print(bootsPercentageList)
+    print(relicPercentageList)
+    print(coreItemsPercentageList)
+    embedVar.set_thumbnail(url="https://static.smite.guru/i/champions/icons/{}.jpg".format(godName))
+    embedVar.set_author(name=godName, url="https://smite.guru/builds/{}".format(godName), icon_url="https://static.smite.guru/i/champions/icons/{}.jpg".format(godName))
+    embedVar.add_field(name="Popular Blessings", value=starterList[0] + ": " + starterPercentageList[0] + "\n"
+                       + starterList[1] + ": " + starterPercentageList[1] + "\n"
+                       + starterList[2] + ": " + starterPercentageList[2],
+                       inline=True)
+    embedVar.add_field(name="Popular Boots", value=bootsList[0] + ": " + bootsPercentageList[0] + "\n"
+                       + bootsList[1] + ": " + bootsPercentageList[1] + "\n"
+                       + bootsList[2] + ": " + bootsPercentageList[2],
+                       inline=True)
+    embedVar.add_field(name="Popular Relics", value=relicList[0] + ": " + relicPercentageList[0] + "\n"
+                       + relicList[1] + ": " + relicPercentageList[1] + "\n"
+                       + relicList[2] + ": " + relicPercentageList[2],
+                       inline=False)
+    embedVar.add_field(name="Popular Core Items", value=coreItemsList[0] + ": " + coreItemsPercentageList[0] + "\n"
+                       + coreItemsList[1] + ": " + coreItemsPercentageList[1] + "\n"
+                       + coreItemsList[2] + ": " + coreItemsPercentageList[2] + "\n"
+                       + coreItemsList[3] + ": " + coreItemsPercentageList[3],
+                       inline=True)
+    return embedVar
+
+
+def mathStuff(equation):
+    finalValue = 0
     num1 = float(equation[0])
     num2 = float(equation[2])
     operator = equation[1]
@@ -30,18 +79,36 @@ def math(equation):
     return finalValue
 
 
-def pull_build(godName):
+def pull_build_data(godName):
     site_url = "http://smite.guru"
-    site_dir = "/builds/" + godName[0]
+    site_dir = "/builds/" + godName
     site_html = requests.get(site_url + site_dir).text
     soup = BeautifulSoup(site_html, "html.parser")
 
-    # print("For {}, the following items are most popular".format(godName[0]))
+    # itemList = []
+    # for item in soup.select(".primary-item img"):
+    #     itemList.append(item["alt"])
+    # return itemList
 
+    # for string in soup.find_all("span"):
+    #     print(soup.span.string)
+
+    itemsAndPercentagesLists = []
     itemList = []
-    for item in soup.select(".primary-item img"):
+    percentageList = []
+    spanList = soup.select("span")
+    for number in range(44, 58):
+        percentageList.append(spanList[number].string)
+    print(percentageList)
+    print(len(percentageList))
+    #print(spanList[44].string)
+    for item in soup.select(".item.item-row__img img"):
         itemList.append(item["alt"])
-    return itemList
+    print(itemList)
+    print(len(itemList))
+    itemsAndPercentagesLists.append(itemList)
+    itemsAndPercentagesLists.append(percentageList)
+    return itemsAndPercentagesLists
 
 
 if __name__ == '__main__':
@@ -62,11 +129,14 @@ if __name__ == '__main__':
 
     @client.event
     async def on_message(message):
+        if message.author == client.user:
+            return
+
         if message.content.startswith("$saymyname"):
             await message.channel.send(message.author)
 
-        if message.author == client.user:
-            return
+        if message.content.startswith("dab"):
+            await message.channel.send(":Dab:")
 
         if message.content.startswith("$hello"):
             await message.channel.send("Hello friends")
@@ -74,12 +144,33 @@ if __name__ == '__main__':
         if message.content.startswith("$math"):
             equation = message.content.split()
             equation.pop(0)
-            await message.channel.send(math(equation))
+            await message.channel.send(mathStuff(equation))
+
+        if message.content.startswith('$test'):
+            embedVar = discord.Embed(color=0x7fffd4)
+            #embedVar.set_thumbnail(url="https://static.smite.guru/i/champions/icons/eset.jpg")
+            embedVar.set_author(name="Eset", url="https://smite.guru/builds/eset", icon_url="https://static.smite.guru/i/champions/icons/eset.jpg")
+            # embedVar.set_image(url="https://static.scientificamerican.com/sciam/cache/file/41DF7DA0-EE58-4259-AA815A390FB37C55_source.jpg")
+            # embedVar.set_image(url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png")
+            embedVar.add_field(name="Build", value="<:veryaveragedab:828016535708237875>", inline=True)
+            await message.channel.send(embed=embedVar)
 
         if message.content.startswith("$build"):
-            godName = message.content.split()
-            godName.pop(0)
-            itemList = pull_build(godName)
+            messageContent = message.content.split()
+            messageContent.pop(0)
+            print(messageContent)
+            if len(messageContent) > 1:
+                unformattedGodName = ""
+                for part in messageContent:
+                    unformattedGodName = unformattedGodName + part + " "
+                unformattedGodName.strip()
+                print(unformattedGodName)
+                godName = godAbbreviations(unformattedGodName)
+            else:
+                godName = godAbbreviations(messageContent[0])
+            print(godName)
+            itemList = pull_build_data(godName)[0]
+            percentageList = pull_build_data(godName)[1]
             itemString = ""
             for item in itemList:
                 if item == itemList[0]:
@@ -89,10 +180,11 @@ if __name__ == '__main__':
             if itemString == "":
                 await message.channel.send("Not a god lol")
             else:
-                await message.channel.send(itemString)
+                itemBuildEmbed = createGuidedBuildEmbed(godName, itemList, percentageList)
+                await message.channel.send(embed=itemBuildEmbed)
 
         if message.content.startswith("$ban"):
             await message.channel.send("Priveledges have been revoked from Bean Bean #5753")
 
 
-    client.run("ODI2NDcyMTM2Njg3NzQ3MTMy.YGM-KA.ksiz7wi6yGxJpfd24x6YkRn_mSY")
+    client.run(token)
